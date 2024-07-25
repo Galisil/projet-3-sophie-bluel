84153a37095d376f712053d2e83424ce213269f4
Galisil <juliette.maret@gmail.com>
2024-07-25 14:21:12 +0200

    synchro supp galerie fond direct après ajout modal

--- FrontEnd/script-modal.js
+++ FrontEnd/script-modal.js
@@ -47,7 +47,7 @@ async function generatePhotosWorks() {
 async function synchroSuppGallery(btnSuppId, figureToSupp) {
     const gallery = document.querySelector(".gallery");
     const allFigures = gallery.querySelectorAll("figure");
-    figureToSupp = null;
+    //figureToSupp = null;
     for (let i = 0; i < allFigures.length; i++) {
         let figure = allFigures[i];
         if (figure.id === btnSuppId) {
@@ -101,19 +101,19 @@ function openSecondPage(event) {
     }
 }
 
-/*function backToFirstPage(event) {
+function backToFirstPage(event) {
     event.preventDefault();
     if (modal) {
         modalWrapper1.setAttribute("aria-modal", "true");
         modalWrapper2.style.display = "none";
         modalWrapper1.style.display = "flex";
     }
-}*/
+}
 
 async function deleteWorks(
     toto,
-    divImgAndSupp,
-    figureToSupp /*, figureToSupp, gallery*/
+    divImgAndSupp /*, figureToSupp, gallery*/
+    /*figureToSupp*/
 ) {
     /*const gallery = document.querySelector(".gallery");
     const figure = gallery.querySelectorAll("figure");
@@ -153,16 +153,22 @@ document.addEventListener("DOMContentLoaded", () => {
     modalWrapper1.addEventListener("click", stopPropagation);
     modalWrapper2.addEventListener("click", stopPropagation);
     if (btnPostPhoto) {
-        btnPostPhoto.addEventListener("click", openSecondPage);
+        btnPostPhoto.addEventListener(
+            "click",
+            //ReverseViewImgSelected,
+            openSecondPage
+        );
     }
     if (btnGoBack) {
-        btnGoBack.addEventListener("click", openModal /* ou backToFirstPage*/);
+        btnGoBack.addEventListener("click", backToFirstPage);
     }
     if (fileUpload) {
         fileUpload.addEventListener("change", viewImgSelected);
     }
+    let figureToAdd = null;
     if (formAddWork) {
         formAddWork.addEventListener("submit", submitForm);
+        //synchroAddGallery(figureToAdd);
         console.log(formAddWork);
     }
     //formAddWork.addEventListener("submit", logSubmit);
@@ -207,6 +213,28 @@ function viewImgSelected(event) {
     }
 }
 
+// construction fonction pour nettoyer la div containrer photo quand on quitte la secondemodale
+/*function ReverseViewImgSelected(event) {
+    const file = event.target.files[0];
+    const fileUpload = document.getElementById("fileUpload");
+    if (file) {
+        //const reader = new FileReader();
+        //reader.onload = function (event) {
+        let containerPhoto = document.querySelector(".containerPhoto");
+        let selectedImg = document.getElementById("customTest");
+        let iconeImage = document.getElementById("iconeImage");
+        let textContainerPhoto = document.getElementById("textContainerPhoto");
+        if (selectedImg) {
+            iconeImage.style = "display:inline";
+            textContainerPhoto.style = "display:inline";
+            containerPhoto.style.cssText = "";
+            selectedImg.style.cssText = "";
+            selectedImg.innerHTML = `""`;
+            console.log(selectedImg);
+        }
+    }
+}*/
+
 //////
 
 /* */
@@ -217,6 +245,11 @@ async function submitForm(event) {
     //const selectedImg = document.getElementById("selectedImg");
     let fileUpload = document.getElementById("fileUpload");
     let file = fileUpload.files[0];
+    let figureToAdd = null;
+    let addPhotoTitleInput = document.getElementById("addPhotoTitleInput");
+    let addPhotoCategoryInput = document.getElementById(
+        "addPhotoCategoryInput"
+    );
     //);
     const formData = new FormData(formAddWork);
     console.log(formData.entries());
@@ -241,33 +274,125 @@ async function submitForm(event) {
         /////
         if (result.ok) {
             console.log("tout est ok");
+            const figureToAdd = await result.json();
             synchroAddGallery(figureToAdd);
+            synchroAddGalleryModal(figureToAdd);
+            backToFirstPage(event);
+            console.log(figureToAdd);
         } else {
             console.log("jpp");
+            let contentMsgError =
+                "Tous les champs doivent être remplis pour pouvoir valider";
+            let msgError = document.createElement("p");
+            msgError.textContent = contentMsgError;
+            //Supprimer le message d'erreur précédent s'il existe
+            const existingError = formAddWork.querySelector("p.error-message");
+            if (existingError) {
+                formAddWork.removeChild(existingError);
+            } else if (
+                //file === null ||
+                addPhotoTitleInput.value === null ||
+                addPhotoCategoryInput.value === null
+            ) {
+                // Ajout classe "error-message" pour la suppression future
+                msgError.classList.add("error-message");
+                formAddWork.appendChild(msgError);
+                //formAddWork.querySelector("[name=email]").value = "";
+                //formAddWork.querySelector("[name=password]").value = "";
+                //throw new Error("Identifiants incorrects");
+            }
         }
-        if (token) {
-            console.log(token);
-        }
+        //if (token) {
+        //console.log(token);
+        //}
     } catch (error) {
         console.error("Fetch error:", error);
     }
 }
 
-/*
 async function synchroAddGallery(figureToAdd) {
-    const gallery = document.querySelector(".gallery");
-    const allFigures = gallery.querySelectorAll("figure");
-    figureToAdd = null;
+    let gallery = document.querySelector(".gallery");
+    let newFigure = document.createElement("figure");
+    newFigure.id = figureToAdd.id;
+    let imageWork = document.createElement("img");
+    imageWork.src = figureToAdd.imageUrl;
+    imageWork.alt = figureToAdd.title;
+    let nameWork = document.createElement("figcaption");
+    nameWork.innerText = figureToAdd.title;
+    newFigure.appendChild(imageWork);
+    newFigure.appendChild(nameWork);
+    gallery.appendChild(newFigure);
+    //ce qui reste à faire gnagnagna
+    //photosGalleryModal.appendChild(newFigure);
+}
+
+async function synchroAddGalleryModal(figureToAdd) {
+    let divGalleryModal = document.querySelector(".photosGalleryModal");
+    let gallery = document.querySelector(".gallery");
+    let figure = document.querySelectorAll(".figure");
+    let divImgAndSupp = document.createElement("div");
+    divImgAndSupp.className = "divImgAndSupp";
+    divImgAndSupp.id = /*works[0]*/ figureToAdd.id;
+    //création d'un attribut catégorie pour chaque photo
+    //let category = works[i].category.name;
+    //divImgAndSupp.setAttribute("category", category);
+    //
+    let photos = document.createElement("img");
+    photos.src = figureToAdd.imageUrl;
+    photos.className = "photosWorksGalleryModal";
+    //création btn supp pour chaque photo
+    let btnSupp = document.createElement("button");
+    btnSupp.className = "btnSupp";
+    btnSupp.id = divImgAndSupp.id;
+    figure.id = divImgAndSupp.id;
+    //ajout icone poubelle pour chaque btn supp
+    let iconSupp = document.createElement("img");
+    iconSupp.src = "./assets/icons/icone-poubelle.svg";
+    iconSupp.alt = "Supprimer la photo";
+    btnSupp.appendChild(iconSupp);
+    //ajout de tout le bazar dans la galerie
+    divImgAndSupp.appendChild(photos);
+    divImgAndSupp.appendChild(btnSupp);
+    divGalleryModal.appendChild(divImgAndSupp);
+    btnSupp.addEventListener("click", () => {
+        synchroSuppGallery(btnSupp.id, figure);
+        deleteWorks(btnSupp.id, divImgAndSupp /*, figureToSupp, gallery*/);
+    });
+}
+
+/*async function synchroAddGallery(figureToAdd) {
+    const reponseWorks = await fetch("http://localhost:5678/api/works");
+    const works = await reponseWorks.json();
+    let gallery = document.querySelector(".gallery");
+    let allFigures = gallery.querySelectorAll("figure");
     for (let i = 0; i < allFigures.length; i++) {
+        let exemple = works[i];
+        figureToAdd = document.createElement("figure");
+        //newFigure.id = works[i].id;
+        let imageWork = document.createElement("img");
+        imageWork.src = exemple.imageUrl;
+        let nameWork = document.createElement("figcaption");
+        nameWork.innerText = exemple.title;
+        //figureToAdd = null;
+        let maxId = allFigures.length;
+        console.log(allFigures);
+        console.log(maxId);
         let figure = allFigures[i];
-        if (figure.id === btnSuppId) {
-            figureToSupp = figure;
+        if (figure.id < maxId) {
+            figureToAdd = figure;
             break;
         }
+        figureToAdd.appendChild(imageWork);
+        figureToAdd.appendChild(nameWork);
+        gallery.appendChild(figureToAdd);
     }
-    console.log(figureToSupp);
-    gallery.appendChild(figureToAdd);
+    console.log(figureToAdd);
 }
+
+
+///////
+{id: 19, title: 'ezezdsdezezd', imageUrl: 'http://localhost:5678/images/malt-and-juniper-NY1721827816725.png', categoryId: '3', userId: 1}
+///////
 
 /*
 Pour restaurer travaux suprimés dans la base de données (supp travaux dans la modale):
